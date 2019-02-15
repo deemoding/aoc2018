0fe660d8a22e1b81f9d57c0b89f304b5f43fa3ee
deemoding <wsd009811@gmail.com>
2019-02-15 15:33:48 +0800

    finish day1 part2

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,8 +1,29 @@
+use std::collections::HashMap;
+
 pub fn main() {
     let file = include_str!("input");
-    let nums:Vec<&str> = file.split_terminator('\n').collect();
-    let sum:i32 = nums.iter()
-        .map(|x| x.parse::<i32>().expect("trans to i32 error"))
-        .sum();
-    println!("{}", sum);
+    let nums:Vec<i32> = file
+        .split_terminator('\n')
+        .map(|x: &str| x.parse::<i32>().expect("trans to i32 error"))
+        .collect();
+    let sum:i32 = nums.iter().sum();
+    println!("part1: {}", sum);
+
+    let mut reach_twice:HashMap<i32, bool> = HashMap::new();
+    let mut sum:i32 = 0;
+    let mut i = 0;
+    let len = nums.len();
+    loop {
+        sum += &nums[i];
+        if reach_twice.get(&sum).is_some() {
+            println!("part2: {}", sum);
+            break;
+        } else {
+            reach_twice.insert(sum, true);
+            i += 1;
+            if i >= len {
+                i = 0;
+            }
+        }
+    }
 }
