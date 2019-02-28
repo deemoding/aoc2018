2b9926f383be479bf9822ff3b0c804f59b5e5cb5
deemoding <wsd009811@gmail.com>
2019-02-28 18:20:35 +0800

    finish day5 part2

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -1,7 +1,8 @@
-pub fn main() {
-    let file = include_str!("input");
-    let mut s = String::from(file);
+use regex::Regex;
+
+fn react(polymer: &String) -> usize {
     let mut point:usize = 0;
+    let mut s = String::clone(polymer);
     while point < s.len() - 1 {
         if (&s[point..point + 1].to_lowercase() == &s[point + 1..point + 2].to_lowercase())
             && (&s[point..point + 1] != &s[point + 1..point + 2]) {
@@ -13,5 +14,28 @@ pub fn main() {
         }
     }
     // 忽略结尾的换行符号
-    println!("part1: {}", s.len() - 1);
+    return s.len() - 1;
+}
+
+pub fn main() {
+    let file = include_str!("input");
+    let s = String::from(file);
+    println!("part1: {}", react(&s));
+
+    let mut min:usize = 99999;
+    for i in b'a'..=b'z' {
+        let reg = format!("(?i){}*", String::from_utf8(vec![i]).unwrap());
+        let re = Regex::new(&reg).unwrap();
+        let length = react(&re.replace_all(&s, "").to_string());
+        /* 解法2
+    for i in (b'a'..=b'z').map(|c| (c as char)) {
+        let mut tmp = String::clone(&s);
+        tmp.retain(|c| c != i && c.to_string() != i.to_uppercase().to_string());
+        let length = react(&tmp);
+        */
+        if length < min {
+            min = length;
+        }
+    }
+    println!("part2: {}", min);
 }
